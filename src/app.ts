523b83b354bef8360a13bdb325c50f04972abd14
Tahmid <tahmid.chowdhury@sjinnovation.com>
2025-09-06 03:43:40 +0600

    worked on the service and global error handler for now ignore apperror and notFOund file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express, { Request, Response } from "express"
 import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +16,10 @@ app.get('/', (req: Request, res: Response) => {
     message: 'Welcome To Tour Management System Backend',
   });
 });
+
+app.use(globalErrorHandler)
+
+app.use(notFound)
 
 
 export default app;
